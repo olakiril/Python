8717b7694355c736946fad03dcd8bc7552dd67b1
Emmanouil Froudarakis <olakiril@hotmail.com>
2016-06-20 20:02:50 -0500

    better

--- state.py
+++ state.py
@@ -5,25 +5,21 @@ from licked import *
 #from subj import Sessions, Trials, Licks
 from stimulus import *
 import pygame
-
-
 import os
-os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (0,0)
+from glob import glob
+
+os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (0, 0)
 
 # Set Parameters
 w = 480
 h = 320
-trial = 1
 max_trials = 1
-RP = 2
+RP = 4
 ITI = 2
 
 # Set images
-GO_images = ('images/obj1_01.jpeg', 'images/obj1_02.jpeg')
-NOGO_images = ('images/obj2_01.jpeg', 'images/obj2_02.jpeg')
-
-#GO_images = ("images/obj1_01.jpeg", "images/obj1_02.jpeg", "images/obj1_03.jpeg", "images/obj1_04.jpeg", "images/obj1_05.jpeg", "images/obj1_06.jpeg", "images/obj1_07.jpeg", "images/obj1_08.jpeg", "images/obj1_09.jpeg", "images/obj1_10.jpeg", "images/obj1_11.jpeg", "images/obj1_12.jpeg", "images/obj1_13.jpeg", "images/obj1_14.jpeg", "images/obj1_15.jpeg", "images/obj1_16.jpeg", "images/obj1_17.jpeg", "images/obj1_18.jpeg", "images/obj1_19.jpeg", "images/obj1_20.jpeg")
-#NOGO_images = ("images/obj2_01.jpeg", "images/obj2_02.jpeg", "images/obj2_03.jpeg", "images/obj2_04.jpeg", "images/obj2_05.jpeg", "images/obj2_06.jpeg", "images/obj2_07.jpeg", "images/obj2_08.jpeg", "images/obj2_09.jpeg", "images/obj2_10.jpeg", "images/obj2_11.jpeg", "images/obj2_12.jpeg", "images/obj2_13.jpeg", "images/obj2_14.jpeg", "images/obj2_15.jpeg", "images/obj2_16.jpeg", "images/obj2_17.jpeg", "images/obj2_18.jpeg", "images/obj2_19.jpeg", "images/obj2_20.jpeg")
+GO_images = glob('images/obj1*')
+NOGO_images = glob('images/obj2*')
 
 # Initialize image matrices
 GO_resp = np.ones(np.size(GO_images))
@@ -35,14 +31,15 @@ ALL_resp = np.concatenate([GO_resp, NOGO_resp])
 stim = stimulus(ALL_images, w, h)
 
 # Datajoin stuff
-timer.start()
 #session = dict(mouse_id=1, session_tmst=timer.start_time)
 #Sessions().insert1(session)
 
 # Initialize other settings
+timer.start()
 stim.init_block()
 trial = 0
 state_time = 0
+pygame.mouse.set_visible(0)
 
 # RUN
 while trial < max_trials:  # Each trial is one block
@@ -59,6 +56,11 @@ while trial < max_trials:  # Each trial is one block
         # Start countdown for response
         timer.start()
         while timer.elapsed_time() < RP:  # response period
+            events = pygame.event.get()
+            for event in events:
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    quit()
+
             if licked().lick():
  #               lick_key = dict(lick_tmst=timer.time())
  #               Licks().insert1(dict(session, **lick_key))
