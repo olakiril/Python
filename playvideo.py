dbdfcf1b4866003d041e925c804b4c6dcdc1342a
manolis <olakiril@hotmail.com>
2016-12-07 13:04:08 -0600

    no memory

--- playvideo.py
+++ playvideo.py
@@ -3,6 +3,7 @@
 #from pipeline import vis
 #import io, imageio, pygame
 import pygame
+import numpy as np
 
 #key = dict([('animal_id', 9036), ('session', 1), ('scan_idx', 3), ('psy_id', 1), ('trial_idx', 6118), ('cond_idx', 14)])
 #clip_info = (vis.Trial()*vis.MovieClipCond()*vis.Movie.Clip() & key).fetch1()
@@ -10,16 +11,21 @@ import pygame
 
 size = (256, 144)
 
-screen = pygame.display.set_mode(size)
 pygame.init()
-for iframe in range(10): #range(vid.get_length()):
+screen = pygame.display.set_mode((400,400),0,32)
+
+for iframe in range(100):  # range(vid.get_length()):
 
 #    py_image = pygame.image.frombuffer(vid.get_data(iframe), size, "RGB")
-    screen.fill((0,255-iframe*10,iframe*10))
+#    screen.fill((0,255-iframe*10,iframe*10))
 #    screen.blit(py_image, (0, 0))
 
-    pygame.display.flip()
-    pygame.time.wait(100)
+#   pygame.display.flip()
+#    pygame.time.wait(100)
+
+     screen.fill((np.round(np.random.rand(1)*255),np.round(np.random.rand(1)*255),np.round(np.random.rand(1)*255)))
+#    screen.blit(label, (10,10))
+     pygame.display.update()
 
 pygame.quit()
 quit()
